a01c5b7461d084cdb721f065c4443d06e29d377f
Mohd Raza <noorinfotechinc@gmail.com>
2020-02-28 08:11:38 -0600

    changes

--- api/routes/common-routes.js
+++ api/routes/common-routes.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const Classes = require("../../database/db-models/class-model.js");
 
+router.use("/:id", validateClassId);
+
 router.get("/", (req, res) => {
   Classes.getClass()
     .then(myClasses => {
@@ -12,7 +14,7 @@ router.get("/", (req, res) => {
 });
 
 router.get("/:id", (req, res) => {
-  const { id } = req.params;
+  const { id } = req.class;
   Classes.getClassById(id)
     .then(myClass => {
       res.json(myClass);
@@ -28,4 +30,18 @@ router.post("/search", (req, res) => {
     })
     .catch(err => res.status(500).json({ message: "Failed to get classes" }));
 });
+
+function validateClassId(req, res, next) {
+  const { id } = req.params;
+  Classes.getClassById(id)
+    .then(myClass => {
+      if (myClass) {
+        req.class = myClass;
+        next();
+      } else {
+        res.status(404).json({ message: "invalid class id" });
+      }
+    })
+    .catch(err => res.status(500).json({ message: "exception", err }));
+}
 module.exports = router;
